a0ce34cea86b77898e2cbfaa293a5d30e5e33e77
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2021-07-28 13:44:26 -0300

    main nav

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import styled from 'styled-components'
+import React from 'react';
+import { Logo } from '../src/theme/Logo';
+import { MenuWrapper } from '../src/components/commons/menu/styles/MenuWrapper';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-export default function Home() {
-  return <Title>My page</Title>
-}
+export default function Menu() {
+  return (
+    <MenuWrapper>
+      <MenuWrapper.LeftSide> {/* MenuWrapper.LeftSide */}
+        <Logo />
+      </MenuWrapper.LeftSide>
+      <MenuWrapper.CentralSide as="ul"> {/* MenuWrapper.CentralSide */}
+        {[
+          { url: '/', name: 'Home' },
+          { url: '/faq', name: 'Perguntas Frequentes' },
+          { url: '/sobre', name: 'Sobre' },
+        ].map((link) => (
+          <li key={link.url}>
+            <a href={link.url}>
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide> {/* MenuWrapper.RightSide */}
+        <button type="button">
+          Entrar
+        </button>
+        <button type="button">
+          Cadastrar
+        </button>
+      </MenuWrapper.RightSide>
+    </MenuWrapper>
+  );
+} 
