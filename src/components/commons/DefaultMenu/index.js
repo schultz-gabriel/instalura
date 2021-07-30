db273aa7d9698163156c61faeeda4fc38f7183dc
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2021-07-30 13:33:18 -0300

    fonts

--- src/components/commons/DefaultMenu/index.js
+++ src/components/commons/DefaultMenu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo } from '../../../theme/Logo';
+import {Text} from '../../Foundation/Text/index';
 import { Button } from '../Button';
 import { MenuWrapper } from './styles/MenuWrapper';
 
@@ -16,9 +17,9 @@ export default function Menu() {
           { url: '/sobre', name: 'Sobre' },
         ].map((link) => (
           <li key={link.url}>
-            <a href={link.url}>
+            <Text variant="smallestException" tag="a" href={link.url}>
               {link.name}
-            </a>
+            </Text>
           </li>
         ))}
       </MenuWrapper.CentralSide>
@@ -32,4 +33,4 @@ export default function Menu() {
       </MenuWrapper.RightSide>
     </MenuWrapper>
   );
-}
+};
