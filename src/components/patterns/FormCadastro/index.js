5b4ee7c3182d0ae221a221451d972d968732f299
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2021-08-13 21:24:34 -0300

    close button on modal

--- src/components/patterns/FormCadastro/index.js
+++ src/components/patterns/FormCadastro/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Button from '../../commons/Button';
 import TextField from '../../forms/TextField';
@@ -5,7 +6,7 @@ import Box from '../../Foundation/layout/Box';
 import Grid from '../../Foundation/layout/Grid';
 import Text from '../../Foundation/Text';
 
-function FormContent() {
+function FormContent(onClose) {
   const [userInfo, setUserInfo] = React.useState({
     usuario: 'omariosouto',
     email: 'devsoutinho@gmail.com',
@@ -22,62 +23,74 @@ function FormContent() {
   const isFormInvalid = userInfo.usuario.length === 0 || userInfo.email.length === 0;
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log('O formulário ta pronto, vamos cadastrar de fato o usuario');
-      }}
-    >
-
-      <Text
-        variant="title"
-        tag="h1"
-        color="tertiary.main"
-      >
-        Pronto para saber da vida dos outros?
-      </Text>
-      <Text
-        variant="paragraph1"
-        tag="p"
-        color="tertiary.light"
-        marginBottom="32px"
-      >
-        Você está a um passo de saber tudoo que está
-        rolando no bairro, complete seu cadastro agora!
-      </Text>
-
-      <div>
-        <TextField
-          placeholder="Email"
-          name="email"
-          value={userInfo.email}
-          onChange={handleChange} // capturadores, pegadores de ação
-        />
-      </div>
-
-      <div>
-        <TextField
-          placeholder="Usuário"
-          name="usuario"
-          value={userInfo.usuario}
-          onChange={handleChange}
-        />
-      </div>
-
+    <>
       <Button
         variant="primary.main"
-        type="submit"
-        disabled={isFormInvalid}
-        fullWidth
+        position="absolute"
+        top="0"
+        right="0"
+        ghost
+        onClick={() => onClose.onClose()}
       >
-        Cadastrar
+        x
       </Button>
-    </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log('O formulário ta pronto, vamos cadastrar de fato o usuario');
+        }}
+      >
+
+        <Text
+          variant="title"
+          tag="h1"
+          color="tertiary.main"
+        >
+          Pronto para saber da vida dos outros?
+        </Text>
+        <Text
+          variant="paragraph1"
+          tag="p"
+          color="tertiary.light"
+          marginBottom="32px"
+        >
+          Você está a um passo de saber tudoo que está
+          rolando no bairro, complete seu cadastro agora!
+        </Text>
+
+        <div>
+          <TextField
+            placeholder="Email"
+            name="email"
+            value={userInfo.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <TextField
+            placeholder="Usuário"
+            name="usuario"
+            value={userInfo.usuario}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button
+          variant="primary.main"
+          type="submit"
+          disabled={isFormInvalid}
+          fullWidth
+        >
+          Cadastrar
+        </Button>
+      </form>
+    </>
   );
 }
 
 // eslint-disable-next-line react/prop-types
-export default function FormCadastro({ propsDoModal }) {
+export default function FormCadastro({ propsDoModal, onClose }) {
   return (
     <Grid.Row
       marginLeft={0}
@@ -105,7 +118,7 @@ export default function FormCadastro({ propsDoModal }) {
           // eslint-disable-next-line react/jsx-props-no-spreading
           {...propsDoModal}
         >
-          <FormContent />
+          <FormContent onClose={onClose} />
         </Box>
       </Grid.Col>
     </Grid.Row>
