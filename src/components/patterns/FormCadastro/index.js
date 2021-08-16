149cd3f08d88379b52beaed599879d04ad5884b5
SchultzGabriel <gabriel.bridi2008@hotmail.com>
2021-08-16 16:02:12 -0300

    login system

--- src/components/patterns/FormCadastro/index.js
+++ src/components/patterns/FormCadastro/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Button from '../../commons/Button';
 import TextField from '../../forms/TextField';
@@ -5,10 +6,20 @@ import Box from '../../Foundation/layout/Box';
 import Grid from '../../Foundation/layout/Grid';
 import Text from '../../Foundation/Text';
 
-function FormContent() {
+const formStates = {
+  DEFAULT: 'DEFAULT',
+  LOADING: 'LOADING',
+  DONE: 'DONE',
+  ERROR: 'ERROR',
+};
+
+function FormContent(onClose) {
+  const [isFormSubmited, setIsFormSubmited] = React.useState(false);
+  const [submissionStatus, setSubmissionStatus] = React.useState(formStates.DEFAULT);
+
   const [userInfo, setUserInfo] = React.useState({
-    usuario: 'omariosouto',
-    email: 'devsoutinho@gmail.com',
+    usuario: 'omariosouto1002',
+    nome: 'Mario Souto',
   });
 
   function handleChange(event) {
@@ -19,65 +30,119 @@ function FormContent() {
     });
   }
 
-  const isFormInvalid = userInfo.usuario.length === 0 || userInfo.email.length === 0;
+  const isFormInvalid = userInfo.usuario.length === 0 || userInfo.nome.length === 0;
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log('O formulário ta pronto, vamos cadastrar de fato o usuario');
-      }}
-    >
-
-      <Text
-        variant="title"
-        tag="h1"
-        color="tertiary.main"
-      >
-        Pronto para saber da vida dos outros?
-      </Text>
-      <Text
-        variant="paragraph1"
-        tag="p"
-        color="tertiary.light"
-        marginBottom="32px"
-      >
-        Você está a um passo de saber tudoo que está
-        rolando no bairro, complete seu cadastro agora!
-      </Text>
-
-      <div>
-        <TextField
-          placeholder="Email"
-          name="email"
-          value={userInfo.email}
-          onChange={handleChange} // capturadores, pegadores de ação
-        />
-      </div>
-
-      <div>
-        <TextField
-          placeholder="Usuário"
-          name="usuario"
-          value={userInfo.usuario}
-          onChange={handleChange}
-        />
-      </div>
-
+    <>
       <Button
         variant="primary.main"
-        type="submit"
-        disabled={isFormInvalid}
-        fullWidth
+        position="absolute"
+        top="0"
+        right="0"
+        ghost
+        onClick={() => onClose.onClose()}
       >
-        Cadastrar
+        x
       </Button>
-    </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setIsFormSubmited(true);
+          const userDTO = {
+            username: userInfo.usuario,
+            name: userInfo.nome,
+          };
+
+          fetch('https://instalura-api.vercel.app/api/users', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userDTO),
+          })
+            .then((respostaDoServidor) => {
+              if (respostaDoServidor.ok) {
+                return respostaDoServidor.json();
+              }
+              throw new Error('Não foi possível cadastrar o usuário agora :(');
+            })
+            .then((respostaConvertidaEmObjeto) => {
+              setSubmissionStatus(formStates.DONE);
+              // eslint-disable-next-line no-console
+              console.log(respostaConvertidaEmObjeto);
+            })
+            .catch((error) => {
+              setSubmissionStatus(formStates.ERROR);
+              // eslint-disable-next-line no-console
+              console.error(error);
+            });
+        }}
+      >
+
+        <Text
+          variant="title"
+          tag="h1"
+          color="tertiary.main"
+        >
+          Pronto para saber da vida dos outros?
+        </Text>
+        <Text
+          variant="paragraph1"
+          tag="p"
+          color="tertiary.light"
+          marginBottom="32px"
+        >
+          Você está a um passo de saber tudoo que está
+          rolando no bairro, complete seu cadastro agora!
+        </Text>
+
+        <div>
+          <TextField
+            placeholder="Nome"
+            name="nome"
+            value={userInfo.nome}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <TextField
+            placeholder="Usuário"
+            name="usuario"
+            value={userInfo.usuario}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button
+          variant="primary.main"
+          type="submit"
+          disabled={isFormInvalid}
+          fullWidth
+        >
+          Cadastrar
+        </Button>
+        {isFormSubmited && submissionStatus === formStates.DONE && (
+        <Box>
+          Deu tudo certo!
+        </Box>
+        )}
+
+        {isFormSubmited && submissionStatus === formStates.ERROR && (
+        <Box
+          display="flex"
+          justifyContent="center"
+        >
+          Deu tudo errado!
+        </Box>
+        )}
+      </form>
+    </>
   );
 }
 
 // eslint-disable-next-line react/prop-types
-export default function FormCadastro({ propsDoModal }) {
+export default function FormCadastro({ propsDoModal, onClose }) {
   return (
     <Grid.Row
       marginLeft={0}
@@ -105,7 +170,7 @@ export default function FormCadastro({ propsDoModal }) {
           // eslint-disable-next-line react/jsx-props-no-spreading
           {...propsDoModal}
         >
-          <FormContent />
+          <FormContent onClose={onClose} />
         </Box>
       </Grid.Col>
     </Grid.Row>
