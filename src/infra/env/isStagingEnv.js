3abaa14201346200ff2121048329b16b97cb2c8f
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-09-12 21:06:37 -0300

    refactor(test): standardizing cypress

---
+++ src/infra/env/isStagingEnv.js
@@ -0,0 +1,7 @@
+const isServer = typeof window === 'undefined';
+
+const isStagingEnv = isServer
+  ? process.env.NODE_ENV === 'development'
+  : globalThis.location.href.includes('localhost');
+
+export { isStagingEnv as default };
