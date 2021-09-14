578b682d3d50a1d887e5c36c99cf4fae9986fecc
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-09-14 13:49:28 -0300

    feat(tests): adding jest
    Adding jest for testing JavaScript.

---
+++ src/theme/utils/propToStyle/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable func-names */
+import breakpointsMedia from '../breakpointMedia';
+
+export default function propToStyle(propName) {
+  return (props) => {
+    const propValue = props[propName];
+
+    if (typeof propValue === 'string' || typeof propValue === 'number') {
+      return {
+        [propName]: propValue,
+      };
+    }
+
+    if (typeof propValue === 'object') {
+      const breakpoints = {};
+
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+
+      return breakpointsMedia(breakpoints);
+
+      // return breakpointsMedia({
+      //   xs: {
+      //     [propName]: propValue.xs,
+      //   },
+      //   sm: {
+      //     [propName]: propValue.sm,
+      //   },
+      //   md: {
+      //     [propName]: propValue.md,
+      //   },
+      //   lg: {
+      //     [propName]: propValue.lg,
+      //   },
+      //   xl: {
+      //     [propName]: propValue.xl,
+      //   },
+      // });
+    }
+
+    return {};
+  };
+}
