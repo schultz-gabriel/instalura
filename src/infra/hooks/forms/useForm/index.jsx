ddfaf4e1c7ef36c60ed9021977302adb22a9db55
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-09-15 14:42:40 -0300

    feat(login): improving login system with yup

--- src/infra/hooks/forms/useForm/index.jsx
+++ src/infra/hooks/forms/useForm/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-export default function useForm({ initialValues, onSubmit }) {
+export default function useForm({ initialValues, onSubmit, validateSchema }) {
   const [values, setValues] = React.useState(initialValues);
+
+  const [isFormDisabled, setIsFormDisabled] = React.useState(true);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouchedFields] = React.useState({});
+
+  React.useEffect(() => {
+    validateSchema(values)
+      .then(() => {
+        setIsFormDisabled(false);
+        setErrors({});
+      })
+      .catch((err) => {
+        const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
+          const fieldName = currentError.path;
+          const errorMessage = currentError.message;
+          return {
+            ...errorObjectAcc,
+            [fieldName]: errorMessage,
+          };
+        }, {});
+        setErrors(formatedErrors);
+        setIsFormDisabled(true);
+      });
+  }, [values]);
 
   return {
     values,
@@ -16,6 +40,17 @@ export default function useForm({ initialValues, onSubmit }) {
         ...currentValues,
         [fieldName]: value,
       }));
+    },
+    isFormDisabled,
+    errors,
+    touched,
+    handleBlur(event) {
+      const fieldName = event.target.getAttribute('name');
+
+      setTouchedFields({
+        ...touched,
+        [fieldName]: true,
+      });
     },
   };
 }
