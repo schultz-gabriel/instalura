bf5257348ac59a1ee738e8a77d1046547fdffa2c
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-09-17 18:59:36 -0300

    feat(tests): adding tests in cache to the ci processes

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -6,7 +6,7 @@ describe('/pages/app/login/', () => {
   describe('when fill and submit a form login request', () => {
     it('go to the profile page', () => {
       // Pré Teste
-      cy.intercept('https://instalura-api-git-master.omariosouto.vercel.app')
+      cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
         .as('userLogin');
 
       // Cenário
@@ -27,25 +27,4 @@ describe('/pages/app/login/', () => {
         });
     });
   });
-
-  // it === test que estamos fazendo
-  // it('preencha os campos e vá para a página /app/profile', () => {
-  //   cy.intercept('https://instalura-api-git-master.omariosouto.vercel.app')
-  //     .as('userLogin');
-
-  //   const loginScreen = new LoginScreenPageObject(cy);
-  //   loginScreen
-  //     .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
-  //     .submitLoginForm();
-
-  //   cy.url().should('include', '/app/profile');
-
-  //   cy.wait('@userLogin')
-  //     .then((intercept) => {
-  //       const { token } = intercept.response.body.data;
-  //       cy.getCookie('APP_TOKEN')
-  //         .should('exist')
-  //         .should('have.property', 'value', token);
-  //     });
-  // });
 });
