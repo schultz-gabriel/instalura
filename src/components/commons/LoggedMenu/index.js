4fca9c227847625810dc1d919811e8cdc2000584
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-10-08 17:17:25 -0300

    feat(layout): logged pages menu added
    Unfunctional buttons.

--- src/components/commons/LoggedMenu/index.js
+++ src/components/commons/LoggedMenu/index.js
@@ -1,40 +1,54 @@
 import React from 'react';
 import Logo from '../../../theme/Logo';
 import MenuWrapper from './style/LoggedMenuWrapper';
+import Grid from '../../foundations/layout/Grid';
 import TextField from '../../forms/TextField';
 import Button from '../Button';
 
 export default function LoggedMenu() {
   return (
     <MenuWrapper>
-      <MenuWrapper.Logo>
-        <Logo />
-      </MenuWrapper.Logo>
-      <MenuWrapper.Menu>
-        <TextField
-          placeholder="Pesquisar"
-          name="search"
-          type="search"
-          margin="0"
-          divClassName="searchDiv"
-          inputClassName="searchInput"
-        />
-        <Button type="button" ghost variant="secondary.main" className="addPicButton">
-          <img src="/images/add.png" alt="adicionar foto" />
-        </Button>
-        <Button type="button" ghost variant="secondary.main" href="/app/login" order="1">
-          <img src="/images/home.png" alt="home" />
-        </Button>
-        <Button type="button" ghost variant="secondary.main" order="2" className="searchIcon">
-          <img src="/images/search.png" alt="home" />
-        </Button>
-        <Button type="button" ghost variant="secondary.main" order="4">
-          <img src="/images/heart.png" alt="likes" />
-        </Button>
-        <Button type="button" ghost variant="secondary.main" order="5">
-          <img className="userPic" src="/images/nick.jpg" alt="user" />
-        </Button>
-      </MenuWrapper.Menu>
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col
+            value={{ xs: 12, md: 10, lg: 8 }}
+            offset={{ xs: 0, md: 1, lg: 2 }}
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            // flexDirection="column"
+          >
+            <MenuWrapper.Logo>
+              <Logo />
+            </MenuWrapper.Logo>
+            <MenuWrapper.Menu>
+              <TextField
+                placeholder="Pesquisar"
+                name="search"
+                type="search"
+                margin="0"
+                divClassName="searchDiv"
+                inputClassName="searchInput"
+              />
+              <Button type="button" ghost variant="secondary.main" className="addPicButton">
+                <img src="/images/add.png" alt="adicionar foto" />
+              </Button>
+              <Button type="button" ghost variant="secondary.main" href="/app/login" order="1">
+                <img src="/images/home.png" alt="home" />
+              </Button>
+              <Button type="button" ghost variant="secondary.main" order="2" className="searchIcon">
+                <img src="/images/search.png" alt="home" />
+              </Button>
+              <Button type="button" ghost variant="secondary.main" order="4">
+                <img src="/images/heart.png" alt="likes" />
+              </Button>
+              <Button type="button" ghost variant="secondary.main" order="5">
+                <img className="userPic" src="/images/nick.jpg" alt="user" />
+              </Button>
+            </MenuWrapper.Menu>
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
     </MenuWrapper>
   );
 }
--- src/components/commons/LoggedMenu/style/LoggedMenuWrapper.js
+++ src/components/commons/LoggedMenu/style/LoggedMenuWrapper.js
@@ -6,18 +6,22 @@ const MenuWrapper = styled.nav`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* flex-wrap: wrap; */
-  margin-top: 18px;
-  padding-left: 28px;
-  padding-right: 28px;
+  padding: 18px 28px;
+  z-index: 10;
+  position:fixed;
+  top:0;
+  left:0;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background.light.color};
   ${breakpointsMedia({
     md: css`
       justify-content: space-between;
-      margin-top: 32px;
+      /* margin-top: 32px; */
       margin-left: auto;
       margin-right: auto;
-      width: 100%;
-      padding: 0 16px;
+      padding: 18px 28px 0 28px;
+      
+
       /* max-width: 768px; */
     `,
     lg: css`
@@ -30,11 +34,18 @@ const MenuWrapper = styled.nav`
 `;
 
 MenuWrapper.Logo = styled.div`
-  padding: 0;
-  margin: 0;
+  left: 50%;
+  margin: 0 0 0 -50px;
   order: 1;
+  padding: 0;
+  position:relative;
+  width:100px;
+
   ${breakpointsMedia({
     md: css`
+        position:initial;
+        margin:0;
+        left:0;
         width: 131px;
         height: 32px;
       `,
@@ -59,6 +70,7 @@ MenuWrapper.Menu = styled.div`
   width: 100%;
   position: fixed;
   bottom:0;
+  left:0;
   background-color: ${({ theme }) => theme.colors.background.light.color};
   border-radius: 24px 24px 0 0;
 
@@ -67,14 +79,15 @@ MenuWrapper.Menu = styled.div`
   }
 
   .searchInput{
-    background: url('/images/search.png') 10px 12px / 10% no-repeat;
+    background: url('/images/search.png') 5px 7px / 10% no-repeat;
     background-color: ${({ theme }) => theme.colors.background.light.color};
-    padding: 12px 20%;
+    padding: 7px 20%;
+    width:200px;
   }
 
   .searchInput:focus{
     background: none;
-    padding: 12px 16px;
+    padding: 7px 16px;
     transition: ${({ theme }) => theme.transition};
   }
 
@@ -98,7 +111,7 @@ MenuWrapper.Menu = styled.div`
   
   ${breakpointsMedia({
     md: css`
-      width:50%;
+      width:60%;
       margin: 0;
       padding-top: 0;
       padding-bottom: 0;
@@ -119,7 +132,7 @@ MenuWrapper.Menu = styled.div`
   }
 
   a, button{
-    width:35px;
+    width:30px;
   }
     `,
   })}
